f73b2461433bc353fc4b3303e07e244173857428
Wilker <Wilker.rj@gmail.com>
2016-06-17 02:37:30 -0300

    Servidor alterado para se conectar ao proxy

---
+++ src/Proxy/ClientRedir.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proxy;
+
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Classe que vai tratar o redirecionamento dos clientes com base em arquivo.
+ * Ela vai ler do arquivo e armazenar uma lista de onde e para qual servidor
+ * será redirecionado.
+ *
+ * @author wilker
+ */
+public class ClientRedir extends readFromFile {
+
+    public ClientRedir() {
+        this.fileName = "clientToServer";
+    }
+
+    /**
+     * Lê o ip de origem recebido, busca na lista de redirecionamento aquele ip
+     * caso esteja na lista, retorna o nome do servidor para qual será
+     * redirecionado caso não esteja, retorna o servidor padrão.
+     *
+     * @param nameServer
+     * @return
+     */
+    String getIpRedir(String nameServer) {
+        this.readServersFromFile();
+        for (String string : servers.keySet()) {
+            if (string.equals(nameServer)) {
+                return servers.get(string);
+            }
+        }
+        return defaultServer;
+    }
+
+    @Override
+    void setDefaultServer(String defaultServer) {
+        this.defaultServer = "default";
+    }
+}
---
+++ src/Proxy/readFromFile.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proxy;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * SuperClasse que trata da abertura e leitura do arquivo e carrega os dados do
+ * no hashmap se o nome do servidor for default, ele é adicionado como servidor
+ * padrão das requisições.
+ *
+ * @author wilker
+ */
+abstract class readFromFile {
+
+    Scanner in;
+    String filePath;
+    String fileName;
+    String defaultServer;
+    Map<String, String> servers = new HashMap<String, String>();
+
+    void readServersFromFile() {
+        try {
+            filePath = System.getProperty("user.dir" + "/" + fileName);
+            in = new Scanner(new File(filePath));
+            do {
+                String s = in.nextLine();
+                if (!(s.charAt(0) == '#')) {
+                    String[] tmp = s.split(" ");
+                    if (tmp[0].equals("default")) {
+                        setDefaultServer(tmp[1]);
+                    } else {
+                        servers.put(tmp[0], tmp[1]);
+                    }
+                }
+            } while (in.hasNextLine());
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("Server List file not found\n");
+
+        }
+    }
+
+    abstract void setDefaultServer(String defaultServer);
+}
